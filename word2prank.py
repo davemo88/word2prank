efdf71a56ec5ce491edee6fe13a54476fa6c75ad
davemo88 <davidkasofsky@gmail.com>
2016-02-13 18:39:00 -0500

    word2prank.py

---
+++ word2prank.py
@@ -0,0 +1,33 @@
+"""
+    ?
+"""
+
+import numpy
+import click
+import pickle
+
+@click.argument('countsfile')
+@click.option('--outfile', default='results.pkl', help='output file')
+@click.option('--dimension', default=100, help='dimension of embeddings')
+@click.option('--iterations', default=10, help='number of iterations')
+def word2prank(countsfile, outfile, dimension, iterations):
+    """
+
+    """
+
+    counts = pickle.loads(countsfile)
+    vocab = counts.keys()
+    num_words = len(vocab)
+    reverse_vocab = {vocab[i] : i for i in range(num_words)}
+    em = numpy.random.rand(num_words, dimension)
+
+    for i in range(iterations):
+        next_em = numpy.zeros((num_words, dimension))
+        for j in range(num_words):
+            for word, count in counts[vocab[j]]:
+                next_em[j] += em[reverse_vocab[word]] * count
+
+        em = next_em
+
+    word_embeddings = {word : em[reverse_vocab[word]] for word in vocab}
+    pickle.dumps(em, word_embeddings)
